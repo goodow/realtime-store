f547367fc82fe230e2d245c0bb92b2ec1ddaca23
Larry Tin <dev@goodow.com>
2015-03-07 00:24:51 +0800

    Update GWT to 2.7.0

--- gwt-compile.sh
+++ gwt-compile.sh
@@ -7,7 +7,20 @@ set -ev
 #    -Dgwt.extraJvmArgs="-Xmx2048M"
 # -Dgwt.draftCompile=true -Dgwt.style=DETAILED -Dgwt.compiler.compileReport=true
 
-java -cp ./target/classes:./src/main/java:$HOME/.m2/repository/com/google/gwt/gwt-user/2.7.0-SNAPSHOT/gwt-user-2.7.0-SNAPSHOT.jar:$HOME/.m2/repository/com/google/gwt/gwt-dev/2.7.0-SNAPSHOT/gwt-dev-2.7.0-SNAPSHOT.jar:$HOME/.m2/repository/javax/validation/validation-api/1.0.0.GA/validation-api-1.0.0.GA.jar:$HOME/.m2/repository/javax/validation/validation-api/1.0.0.GA/validation-api-1.0.0.GA-sources.jar:$HOME/.m2/repository/org/json/json/20090211/json-20090211.jar:$HOME/.m2/repository/com/goodow/realtime/realtime-json/0.5.5-SNAPSHOT/realtime-json-0.5.5-SNAPSHOT.jar:$HOME/.m2/repository/com/goodow/realtime/realtime-json/0.5.5-SNAPSHOT/realtime-json-0.5.5-SNAPSHOT-sources.jar:$HOME/.m2/repository/com/goodow/realtime/realtime-operation/0.5.5-SNAPSHOT/realtime-operation-0.5.5-SNAPSHOT.jar:$HOME/.m2/repository/com/goodow/realtime/realtime-operation/0.5.5-SNAPSHOT/realtime-operation-0.5.5-SNAPSHOT-sources.jar:$HOME/.m2/repository/com/goodow/realtime/realtime-channel/0.5.5-SNAPSHOT/realtime-channel-0.5.5-SNAPSHOT.jar:$HOME/.m2/repository/com/goodow/realtime/realtime-channel/0.5.5-SNAPSHOT/realtime-channel-0.5.5-SNAPSHOT-sources.jar \
+CLASSPATH=./target/classes:./src/main/java
+CLASSPATH=$CLASSPATH:$HOME/.m2/repository/com/google/gwt/gwt-dev/2.7.0/gwt-dev-2.7.0.jar
+CLASSPATH=$CLASSPATH:$HOME/.m2/repository/org/ow2/asm/asm/5.0.3/asm-5.0.3.jar
+CLASSPATH=$CLASSPATH:$HOME/.m2/repository/com/google/gwt/gwt-user/2.7.0/gwt-user-2.7.0.jar
+CLASSPATH=$CLASSPATH:$HOME/.m2/repository/javax/validation/validation-api/1.0.0.GA/validation-api-1.0.0.GA.jar
+CLASSPATH=$CLASSPATH:$HOME/.m2/repository/javax/validation/validation-api/1.0.0.GA/validation-api-1.0.0.GA-sources.jar
+CLASSPATH=$CLASSPATH:$HOME/.m2/repository/com/goodow/realtime/realtime-json/0.5.5-SNAPSHOT/realtime-json-0.5.5-SNAPSHOT.jar
+CLASSPATH=$CLASSPATH:$HOME/.m2/repository/com/goodow/realtime/realtime-json/0.5.5-SNAPSHOT/realtime-json-0.5.5-SNAPSHOT-sources.jar
+CLASSPATH=$CLASSPATH:$HOME/.m2/repository/com/goodow/realtime/realtime-operation/0.5.5-SNAPSHOT/realtime-operation-0.5.5-SNAPSHOT.jar
+CLASSPATH=$CLASSPATH:$HOME/.m2/repository/com/goodow/realtime/realtime-operation/0.5.5-SNAPSHOT/realtime-operation-0.5.5-SNAPSHOT-sources.jar
+CLASSPATH=$CLASSPATH:$HOME/.m2/repository/com/goodow/realtime/realtime-channel/0.5.5-SNAPSHOT/realtime-channel-0.5.5-SNAPSHOT.jar
+CLASSPATH=$CLASSPATH:$HOME/.m2/repository/com/goodow/realtime/realtime-channel/0.5.5-SNAPSHOT/realtime-channel-0.5.5-SNAPSHOT-sources.jar
+
+java -cp $CLASSPATH \
     com.google.gwt.dev.Compiler -war target/realtime-store-0.5.5-SNAPSHOT \
     -XnoclassMetadata -XnocheckCasts -XjsInteropMode JS -XclosureCompiler -draftCompile \
     com.goodow.realtime.store.StoreProd
